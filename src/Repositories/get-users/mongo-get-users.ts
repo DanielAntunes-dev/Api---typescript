ce3c579535ceccd17c6a014d08b5c62df9b61ced
Daniel Junior <dap.junioruvv@gmail.com>
2023-10-04 10:37:07 -0300

    feat: ensure GetUsersRepository gets users from mongo

--- src/Repositories/get-users/mongo-get-users.ts
+++ src/Repositories/get-users/mongo-get-users.ts
@@ -1,29 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Daniel",
-        lastName: "Antunes",
-        email: "daniel@email.com",
-        password: "123456",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
-
-
-// export class PostgresGetUsersRepository implements IGetUsersRepository {
-//   async getUsers(): Promise<User[]> {
-//     return [
-//       {
-//         firstName: "Daniel",
-//         lastName: "Antunes",
-//         email: "daniel@email.com",
-//         password: "123456",
-//       },
-//     ];
-//   }
-// }
