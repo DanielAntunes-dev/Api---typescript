667d953227f87050111c5a0175681dbf4b49da6a
Daniel Junior <dap.junioruvv@gmail.com>
2023-10-04 10:11:17 -0300

    feat: add MongoClient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,30 @@
 import express from "express";
 import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users";
-import { MongoGetUsersRepository, PostgresGetUsersRepository } from './Repositories/get-users/mongo-get-users';
-
-config();
-
-const app = express();
-
-const port = process.env.PORT || 8000;
+import { MongoGetUsersRepository } from "./Repositories/get-users/mongo-get-users";
+import { MongoClient } from "./database/mongo";
 
 // MONGODB
-app.get("/users",async (req, res) => {
-  const mongoGetUsersRepository = new MongoGetUsersRepository()
-  const getUsersController = new GetUsersController(mongoGetUsersRepository)
 
-  const {body, statusCode} = await getUsersController.handle()
+const main = async () => {
+  config();
 
-  res.send(body).status(statusCode)
-});
+  const app = express();
 
-// POSTGRES
-app.get("/users", async (req, res) => {
-  const postgresGetUsersRepository = new PostgresGetUsersRepository();
-  const getUsersController = new GetUsersController(postgresGetUsersRepository);
+  await MongoClient.connect();
 
-  const { body, statusCode } = await getUsersController.handle();
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
-  res.send(body).status(statusCode);
-});
+    const { body, statusCode } = await getUsersController.handle();
 
+    res.send(body).status(statusCode);
+  });
 
-app.listen(port, () => console.log(`Linstening on port ${port}!`));
+  const port = process.env.PORT || 8000;
+
+  app.listen(port, () => console.log(`Linstening on port ${port}!`));
+};
+
+main();
